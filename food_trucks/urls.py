1c476831b63ecec407777847a9053f49949d3831
Manny <manny.talaroc@gmail.com>
2025-01-08 19:18:19 +0800

    removed comments

--- food_trucks/urls.py
+++ food_trucks/urls.py
@@ -3,8 +3,5 @@ from food_trucks.views import NearestFoodTrucks, FoodTruckMapView
 
 urlpatterns = [
     path('', NearestFoodTrucks.as_view(), name='nearest_food_trucks'),
-    path("map/", FoodTruckMapView.as_view(), name="food_trucks_api"),
-    
-    # http://localhost:8000/api/map/?latitude=37.7601&longitude=-122.4188
-
+    path("map/", FoodTruckMapView.as_view(), name="food_trucks_api"),  
 ]
--- food_trucks/views.py
+++ food_trucks/views.py
@@ -1,4 +1,3 @@
-# food_trucks/views.py
 from django.views.generic.base import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
